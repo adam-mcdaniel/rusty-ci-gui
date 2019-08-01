46067850cdc14848f03a6440c28946be8835c161
adam-mcdaniel <adam.mcdanie17@gmail.com>
2019-08-01 12:02:10 -0400

    Removed ListBox and added comments to primitives

--- primitives.py
+++ primitives.py
@@ -1,37 +1,37 @@
+# The goal of this particular file is to provide a means
+# to move to another gui backend. If we do need to switch, 
+# we should be able to reimplement the primitives and 
+# everything else can remain unchanged.
 from tkinter import Tk, Label, Button, Frame, Widget, Entry, Text, Listbox, OptionMenu, StringVar, END
 
 
+# The base class for all Widgets to be displayed
 class GuiWidget(Frame, Widget):
     def __init__(self, master, *args, **kwargs):
         super().__init__(master)
         self.setup(master, *args, **kwargs)
         self.pack()
 
+    # This is to act as the __init__ method for the GuiWidget subclasses
     def setup(self, *args, **kwargs): pass
 
 
-class ListBox(GuiWidget):
-    def setup(self, master, items, *args, **kwargs):
-        self.listbox = Listbox(master)
-        self.listbox.pack()
-
-        for item in items:
-            self.listbox.insert(END, item)
-
-
+# A list of items to choose from as a dropdown
 class DropDown(GuiWidget):
     def setup(self, master, items, *args, **kwargs):
-        self.tkvar = StringVar(master)
+        # Stores current selection
+        self.selection = StringVar(master)
+        self.selection.set(items[0])
 
-        self.tkvar.set(items[0])
-
-        self.popupMenu = OptionMenu(master, self.tkvar, *items)
+        # The menu widget
+        self.popupMenu = OptionMenu(master, self.selection, *items)
         self.popupMenu.pack()
 
-    def __str__(self): return self.tkvar.get()
+    def __str__(self): return self.selection.get()
 
 
-class TextEntry(GuiWidget):
+# Enter one line into a text dialog box
+class LineEntry(GuiWidget):
     def setup(self, master, *args, **kwargs):
         self.content = StringVar()
         self.entry = Entry(master, textvariable=self.content)
@@ -40,6 +40,7 @@ class TextEntry(GuiWidget):
     def __str__(self): return self.content.get()
 
 
+# Enter multiple lines into a dialog box
 class MultilineEntry(GuiWidget):
     def setup(self, master, *args, **kwargs):
         self.entry = Text(master)
